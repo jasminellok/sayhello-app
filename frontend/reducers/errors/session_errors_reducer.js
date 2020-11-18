d9887c976080f935f32d0cc9e3ba367fb88f174c
jasminelok <jasminellok@gmail.com>
2020-11-18 12:15:21 -0500

    added css fonts and colors and added constants to reducers

--- frontend/reducers/errors/session_errors_reducer.js
+++ frontend/reducers/errors/session_errors_reducer.js
@@ -1,13 +1,15 @@
 
 import { RECEIVE_CURRENT_USER, RECEIVE_SESSION_ERRORS, CLEAR_ERRORS } from '../../actions/session_actions';
 
+const defaultErrors = [];
+
 const sessionErrorsReducer = (state = [], action) => {
     Object.freeze(state);
     switch (action.type) {
         case RECEIVE_CURRENT_USER:
-            return [];
+            return defaultErrors;
         case CLEAR_ERRORS:
-            return [];
+            return defaultErrors;
         case RECEIVE_SESSION_ERRORS:
             return action.errors;
         default:
