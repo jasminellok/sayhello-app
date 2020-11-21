4f8f33bf6681ecf90929376e116d5e773c4b567c
jasminelok <jasminellok@gmail.com>
2020-11-21 00:49:56 -0500

    styling nav bar

--- frontend/components/nav/main_nav.jsx
+++ frontend/components/nav/main_nav.jsx
@@ -10,35 +10,68 @@ const home = <FontAwesomeIcon icon={faHome} />
 class MainNavBar extends React.Component {
     constructor(props) {
         super(props)
+        this.state = {
+            showProfile: false
+        }
+        this.toggleDropdown = this.toggleDropdown.bind(this);
+        this.showProfile = this.showProfile.bind(this);
     }
+
+    toggleDropdown(field) {
+        return e => {
+            e.preventDefault();
+            e.stopPropagation();
+            this.setState({[field]: !this.state[field]}, () => {
+                if (this.state[field] === true) { 
+                    document.addEventListener('click', this.toggleDropdown);
+                } else {
+                    document.removeEventListener("click", this.toggleDropdown) 
+
+                }
+            });
+        }
+    }
+
+    showProfile() {
+        return (
+        <div className="profile-links">
+            <span className="accountx">
+                <li> </li>
+                <li> Account </li>
+                <p> x </p>
+            </span>
+            <li className="profile-info">  
+                <p className="profile-name">{this.props.currentUser.full_name}</p>
+                <p className="profile-email">{this.props.currentUser.email}</p>
+            </li>
+            <p className="nav-logout" onClick={this.props.logout}>Log Out</p>
+        </div>
+        )
+    }
+
 
     render () {
         const mainNav = () => (
             <nav className="main-nav">
 
                 <div className="nav-home-btn">
-                    <Link className="home-icon" to="/boards">
-                        <div>{home}</div>
+                    <Link  to="/boards">
+                        <div className="home-icon">{home}</div>
                     </Link>
                 </div>
 
-                {/* <div className="nav-board-index"> Boards
-                    <p>Recent Boards</p>
-                </div> */}
-
-                <div className="go-to-boards">
+                <div className="logo-boards">
                     <Link className="logo-board-index" to="/boards">
                         <img src={window.logo} alt="sayHello" className="nav-logo" /> 
                     </Link>
                 </div>
 
-                <ul className="profile-links"> {this.props.currentUser.full_name[0]}
-                    <li className="profile-info">Account
-                        <p className="profile-name">{this.props.currentUser.full_name}</p>
-                        <p className="profile-email">{this.props.currentUser.email}</p>
-                    </li>
-                    <li className="logout" onClick={this.props.logout}>Log Out</li>
-                </ul>
+                <div className="profile-dropdown-btn" onClick={this.toggleDropdown("showProfile")}>
+                    <p className="profile-letter">
+                        {this.props.currentUser.full_name[0]}
+                    </p>
+                    {this.state.showProfile ? this.showProfile() : null}                    
+                </div>
 
             </nav>);
 
