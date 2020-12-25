a3254aab7e1ef01eb7e58a376a4363e3b3c19043
jasminelok <jasminellok@gmail.com>
2020-12-25 15:26:47 -0500

    Add front end route for BoardUser

--- frontend/util/board_api_util.js
+++ frontend/util/board_api_util.js
@@ -35,9 +35,28 @@ export const deleteBoard = (boardId) => {
     })
 }
 
-        // #        api_boards GET / api / boards(.: format)                                  api / boards#index {: format =>: json }
-        // #                   POST / api / boards(.: format)                                api / boards#create {: format =>: json }
-        // #         api_board GET / api / boards /: id(.: format)                           api / boards#show {: format =>: json }
-        // #                   PATCH / api / boards /: id(.: format)                          api / boards#update {: format =>: json }
-        // #                   PUT / api / boards /: id(.: format)                           api / boards#update {: format =>: json }
-        // #                   DELETE / api / boards /: id(.: format)                      api / boards#destroy {: format =>: json }
+//  api_board_shares POST   /api/board_shares(.:format)                   api/board_shares#create {:format=>:json}
+export const postBoardUser = (boardId) => { //id is board id 
+    return $.ajax({ 
+        method: 'POST',
+        url: '/api/board_shares',
+        data: {boardId},
+    })
+};
+
+//   api_board_share DELETE /api/board_shares/:id(.:format)               api/board_shares#destroy {:format=>:json}
+export const deleteBoardUsers = (boardId) => { //id is board id 
+    return $.ajax({ 
+        method: 'DELETE',
+        url: `/api/board_shares/${boardId}`,
+        data: {boardId},
+        });
+};
+
+
+// #        api_boards GET / api / boards(.: format)                                  api / boards#index {: format =>: json }
+// #                   POST / api / boards(.: format)                                api / boards#create {: format =>: json }
+// #         api_board GET / api / boards /: id(.: format)                           api / boards#show {: format =>: json }
+// #                   PATCH / api / boards /: id(.: format)                          api / boards#update {: format =>: json }
+// #                   PUT / api / boards /: id(.: format)                           api / boards#update {: format =>: json }
+// #                   DELETE / api / boards /: id(.: format)                      api / boards#destroy {: format =>: json }
