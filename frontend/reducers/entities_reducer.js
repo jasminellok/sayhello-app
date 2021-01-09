cf1011f30ac18d96ecb2b69c51c393db413d7baf
jasminelok <jasminellok@gmail.com>
2021-01-08 23:04:38 -0500

    Add comment reducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -4,12 +4,14 @@ import usersReducer from './parts/users_reducer';
 import boardsReducer from './parts/board_reducer';
 import listsReducer from './parts/list_reducer';
 import cardsReducer from './parts/card_reducer';
+import commentsReducer from './parts/comment_reducer';
 
 const entitiesReducer = combineReducers({
     users: usersReducer,
     boards: boardsReducer,
     lists: listsReducer,
     cards: cardsReducer,
+    comments: commentsReducer
 
 })
 
---
+++ frontend/reducers/parts/comment_reducer.js
@@ -0,0 +1,21 @@
+
+import { REC_COMMENT, REC_ALL_COMMENTS, REMOVE_COMMENT } from '../../actions/comment_actions';
+
+const CommentReducer = (state = {}, action) => {
+    Object.freeze(state);
+    let newState = { ...state }
+
+    switch (action.type) {
+        case REC_COMMENT:
+            return Object.assign({}, state, { [action.comment.id]: action.comment })
+        case REC_ALL_COMMENTS:
+            return Object.assign({}, state, action.comments);
+        case REMOVE_COMMENT:
+            delete newState[action.commentId]
+            return newState;
+        default:
+            return state;
+    }
+};
+
+export default CommentReducer;
